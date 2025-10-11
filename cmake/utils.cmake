9a38944a4945c8022d46c8eafc3233fbd1cc6ba2
AlexHendersonEng <alexhendersnao@gmail.com>
2025-10-11 21:34:17 +0100

    Updated add_all_subdirectories function to not have any inputs and add all current directories below current directory

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,16 +1,13 @@
 # Add all subdirectories of a given base directory to the build
-function(add_all_subdirectories base_dir)
-    # Get absolute path of the base directory
-    get_filename_component(abs_base_dir "${base_dir}" ABSOLUTE)
-
+function(add_all_subdirectories)
     # Get list of all subdirectories
-    file(GLOB sub_directories true "${abs_base_dir}" "${abs_base_dir}/*")
+    file(GLOB SUB_DIRECTORIES true "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/*")
 
     # Loop over subdirectories and add them to the build
-    foreach(sub_directory ${sub_directories})
-        if(IS_DIRECTORY "${sub_directory}" AND EXISTS "${sub_directory}/CMakeLists.txt")
-            message(STATUS "Adding subdirectory: ${sub_directory}")
-            add_subdirectory("${sub_directory}")
+    foreach(SUB_DIRECTORY ${SUB_DIRECTORIES})
+        if(IS_DIRECTORY "${SUB_DIRECTORY}" AND EXISTS "${SUB_DIRECTORY}/CMakeLists.txt")
+            message(STATUS "Adding subdirectory: ${SUB_DIRECTORY}")
+            add_subdirectory("${SUB_DIRECTORY}")
         endif()
     endforeach()
 endfunction()
